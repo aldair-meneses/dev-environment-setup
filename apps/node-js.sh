126603f74fff21a06f30ec230ab13bb2b4f60b3e
Aldair Meneses <aldair.meneses@protonmail.com>
2024-07-01 23:26:19 -0300

    add source command in bashrc to enable running the fnm command

--- apps/node-js.sh
+++ apps/node-js.sh
@@ -4,5 +4,7 @@ if ! command -v fnm &>/dev/null; then
 	curl -fsSL https://fnm.vercel.app/install | bash
 fi
 
+source ~/.bashrc
+
 gum spin -s line --title "Installing fnm" -- sleep 0.5
 fnm install --lts
