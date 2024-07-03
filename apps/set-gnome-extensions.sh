8b1736472f5fd0f372ceeb7fcc70c5c2cf44a5ad
Aldair Meneses <aldair.meneses@protonmail.com>
2024-07-03 00:42:28 -0300

    add script to install some gnome extensions and remove some default gnome extensions that comes with Ubuntu

---
+++ apps/set-gnome-extensions.sh
@@ -0,0 +1,24 @@
+if ! command -v pipx &>/dev/null; then
+	echo "pipx not found, it will be installed bofore proceeding with gext installation"
+	sudo apt update
+	sudo apt install pipx gnome-tweak-tools -y
+fi
+
+pipx ensurepath
+
+if ! command -v gext &>/dev/null; then
+	echo "installing gnome-extensions-cli"
+	bash -c "pipx install gnome-extensions-cli --system-site-packages"
+fi
+
+gnome-extensions disable tiling-assistant@ubuntu.com
+gnome-extensions disable ubuntu-dock@ubuntu.com
+gnome-extensions disable ding@rastersoft.com
+
+gum confirm "To install the extensions, please confirm"
+gum style --foreground 212 --border normal --border-foreground 212 --margin "1 2" --padding "2" "Installing Extensions"
+
+bash -c "gext install tactile@lundal.io"
+bash -c "gext install just-perfection-desktop@just-perfection"
+bash -c "gext install blur-my-shell@aunetx"
+bash -c "gext install space-bar@luchrioh"
