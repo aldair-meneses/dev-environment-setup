6c14b59c05bf7c3b4f12f08e52bfd4d3b19ef1db
Aldair Meneses <aldair.meneses@protonmail.com>
2024-07-03 00:02:26 -0300

    refactors script to check if node is installed

--- apps/node-js.sh
+++ apps/node-js.sh
@@ -1,10 +1,14 @@
 gum spin -s line --title "Installing fnm" -- sleep 0.5
 
 if ! command -v fnm &>/dev/null; then
+	gum spin -s line --title "Installing fnm" -- sleep 0.5
 	curl -fsSL https://fnm.vercel.app/install | bash
 fi
 
 source ~/.bashrc
 
-gum spin -s line --title "Installing fnm" -- sleep 0.5
-fnm install --lts
+if ! command -v node &>/dev/null; then
+	fnm install --lts
+else
+	echo "Node is already installed, skipping installation."
+fi
