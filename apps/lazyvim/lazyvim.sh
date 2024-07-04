8603cbdd99917ee3ed15c2628c8663c71de51388
Aldair Meneses <aldair.meneses@proton.me>
2024-07-04 01:45:03 -0300

    update Lazyvim script to install Lazygit and Fira Code font

--- apps/lazyvim/lazyvim.sh
+++ apps/lazyvim/lazyvim.sh
@@ -1,17 +1,24 @@
+#!/bin/bash
+
 gum spin --spinner dot --title "Installing Lazyvim requirements..." -- sleep 1
 
-sudo apt install fzf fd-find neovim ripgrep -y
+sudo apt install fzf fd-find neovim ripgrep fonts-firacode -y
 
+CURRENT_PATH="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 NEOVIM_CONFIG_PATH="$HOME/.config/nvim"
 
 if [ -d "$NEOVIM_CONFIG_PATH.bak" ]; then
-	gum confirm "A backup of nvim already exists. Do you want to replace it with a new backup of the current nvim that is installed?" &&
-		rm -r $NEOVIM_CONFIG_PATH.bak || mv "$NEOVIM_CONFIG_PATH.bak"{,.bak}
+  gum confirm "A backup of nvim already exists. Do you want to replace it with a new backup of the current nvim that is installed?" &&
+    rm -r $NEOVIM_CONFIG_PATH.bak || mv "$NEOVIM_CONFIG_PATH.bak"{,.bak}
 fi
 
-if [ -n "$NEOVIM_CONFIG_PATH" ] && [ -d "$NEOVIM_CONFIG_PATH" ] ; then
-	mv "$NEOVIM_CONFIG_PATH"{,.bak}
+if [ -n "$NEOVIM_CONFIG_PATH" ] && [ -d "$NEOVIM_CONFIG_PATH" ]; then
+  mv "$NEOVIM_CONFIG_PATH"{,.bak}
 fi
 
-git clone https://github.com/LazyVim/starter ~/.config/nvim &&\
-rm -rf ~/.config/nvim/.git
+gum spin --spinner dot --title "Installing Lazygit" -- sleep 1
+
+source "$CURRENT_PATH"/deps/lazygit.sh
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim &&
+  rm -rf ~/.config/nvim/.git
