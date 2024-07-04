be79e59fed73429e8e5329d7a58ff6308e9dd1f5
Aldair Meneses <aldair.meneses@proton.me>
2024-07-04 01:37:43 -0300

    Add workaround to logic of non-interactive shell that is default of Ubuntu based Distros
    This fix is necessary to refresh the environment when happen some export
    in some scripts. Due a rule that Ubuntu based distros have at he
    beginning of ~/.bashrc file:
    
    # If not running interactively, don't do anythin
    ```bash
    case $- in    *i*) ;;
          *)
        return;;
    esac
    
    is not possible to run `source ~/.bashrc`.

--- init.sh
+++ init.sh
@@ -4,6 +4,10 @@ set -e
 CONFIG_DIR="./configs"
 THEME_DIR="./themes"
 
+if [ $(sed -n '/^NAME/p' /etc/os-release | tr -d \"NAME=) = Ubuntu ]; then
+  eval "$(cat ~/.bashrc | tail +10)"
+fi
+
 user_path_string=$(sed '2!d' ./configs/bash/user_local_path)
 if ! grep -q "$user_path_string" ~/.bashrc; then
   cat "$CONFIG_DIR"/bash/user_local_path >>~/.bashrc
@@ -11,41 +15,41 @@ if ! grep -q "$user_path_string" ~/.bashrc; then
 fi
 
 if ! command -v gum &>/dev/null; then
-	echo "Gum.sh will be installed"
-	echo "Updating packages..."
-	sudo apt update -y && sudo apt upgrade -y
-	echo "Installing gum and its dependencies..."
-	sudo apt install curl -y
-	sudo mkdir -p /etc/apt/keyrings
-	curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/charm.gpg
-	echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
-	sudo apt update && sudo apt install gum -y
+  echo "Gum.sh will be installed"
+  echo "Updating packages..."
+  sudo apt update -y && sudo apt upgrade -y
+  echo "Installing gum and its dependencies..."
+  sudo apt install curl -y
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/charm.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+  sudo apt update && sudo apt install gum -y
 fi
 
 CHOICE=$(gum choose "Install Essentials" "Installing Apps" "Setup Lazyvim" "Setup Alacritty" "Quit")
 
 if [ "$CHOICE" == "Quit" ]; then
-	exit 0
+  exit 0
 fi
 
 if [ "$CHOICE" == "Install Essentials" ]; then
-	gum spin -s line --title "Installing Essentials" -- sleep 1
-	source ./libs/essentials.sh
+  gum spin -s line --title "Installing Essentials" -- sleep 1
+  source ./libs/essentials.sh
 fi
 
 if [ "$CHOICE" == "Setup Lazyvim" ]; then
-	APP=lazyvim
-	gum confirm "Do you want to install Lazyvim and its dependencies?" &&
-		source ./apps/$APP/$APP.sh
+  APP=lazyvim
+  gum confirm "Do you want to install Lazyvim and its dependencies?" &&
+    source ./apps/$APP/$APP.sh
 fi
 
 if [ "$CHOICE" == "Setup Alacritty" ]; then
-	APP=alacritty
-	gum confirm "Do you want to install alacritty and its dependencies?" &&
-		source ./apps/$APP/$APP.sh
+  APP=alacritty
+  gum confirm "Do you want to install alacritty and its dependencies?" &&
+    source ./apps/$APP/$APP.sh
 fi
 
 if [ "$CHOICE" == "Installing Apps" ]; then
-	gum spin -s line --title "Installing Apps" -- sleep 1
-	for script in ./apps/*.sh; do source $script; done
+  gum spin -s line --title "Installing Apps" -- sleep 1
+  for script in ./apps/*.sh; do source $script; done
 fi
