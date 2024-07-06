a55c94d71d46409d6d9327b6969f668de53f7963
aldrick <aldair.meneses@proton.me>
2024-07-05 23:59:24 -0300

    Add init recursive function

--- init.sh
+++ init.sh
@@ -25,30 +25,39 @@ if ! command -v gum &>/dev/null; then
   sudo apt update && sudo apt install gum -y
 fi
 
-CHOICE=$(gum choose "Install Essentials" "Installing Apps" "Setup Lazyvim" "Setup Alacritty" "Quit")
+function init() {
 
-if [ "$CHOICE" == "Quit" ]; then
-  exit 0
-fi
+  CHOICE=$(gum choose "Install Essentials" "Installing Apps" "Setup Lazyvim" "Setup Alacritty" "Quit")
 
-if [ "$CHOICE" == "Install Essentials" ]; then
-  gum spin -s line --title "Installing Essentials" -- sleep 1
-  source ./libs/essentials.sh
-fi
+  if [ "$CHOICE" == "Quit" ]; then
+    exit 0
+  fi
 
-if [ "$CHOICE" == "Setup Lazyvim" ]; then
-  APP=lazyvim
-  gum confirm "Do you want to install Lazyvim and its dependencies?" &&
-    source ./apps/$APP/$APP.sh
-fi
+  if [ "$CHOICE" == "Install Essentials" ]; then
+    gum spin -s line --title "Installing Essentials" -- sleep 1
+    source ./libs/essentials.sh
+    init
+  fi
 
-if [ "$CHOICE" == "Setup Alacritty" ]; then
-  APP=alacritty
-  gum confirm "Do you want to install alacritty and its dependencies?" &&
-    source ./apps/$APP/$APP.sh
-fi
+  if [ "$CHOICE" == "Setup Lazyvim" ]; then
+    APP=lazyvim
+    gum confirm "Do you want to install Lazyvim and its dependencies?" &&
+      source ./apps/$APP/$APP.sh
+    init
+  fi
 
-if [ "$CHOICE" == "Installing Apps" ]; then
-  gum spin -s line --title "Installing Apps" -- sleep 1
-  for script in ./apps/*.sh; do source $script; done
-fi
+  if [ "$CHOICE" == "Setup Alacritty" ]; then
+    APP=alacritty
+    gum confirm "Do you want to install alacritty and its dependencies?" &&
+      source ./apps/$APP/$APP.sh
+    init
+  fi
+
+  if [ "$CHOICE" == "Installing Apps" ]; then
+    gum spin -s line --title "Installing Apps" -- sleep 1
+    for script in ./apps/*.sh; do source $script; done
+    init
+  fi
+}
+
+init
