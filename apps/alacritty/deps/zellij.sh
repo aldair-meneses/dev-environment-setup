90a9df2a5b738f5ee13a3720f62167b96d21b34e
aldair-meneses <aldair.meneses@proton.me>
2024-11-24 01:32:19 -0300

    Update zellij configs and themes

--- apps/alacritty/deps/zellij.sh
+++ apps/alacritty/deps/zellij.sh
@@ -1,17 +1,17 @@
 if ! command -v zellij &>/dev/null; then
-	gum spin -s line --title "Installing Zellij" -- sleep 1
-	cd /tmp
-	wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
-	if [ ! -d "/tmp/zellijar" ]; then
-		tar -xf zellij.tar.gz zellij
-	fi
-	sudo install zellij /usr/local/bin
-	rm zellij.tar.gz zellij
-	cd -
+  gum spin -s line --title "Installing Zellij" -- sleep 1
+  cd /tmp
+  wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
+  if [ ! -d "/tmp/zellijar" ]; then
+    tar -xf zellij.tar.gz zellij
+  fi
+  sudo install zellij /usr/local/bin
+  rm zellij.tar.gz zellij
+  cd -
 fi
 
 mkdir -p ~/.config/zellij/
-[ ! -f "$HOME/.config/zellij/config.kdl" ] && cp "$CONFIG_DIR/zellij.kdl" ~/.config/zellij/config.kdl
-[ ! -d "$HOME/.config/zellij/themes" ] && mkdir -p ~/.config/zellij/themes
+[ ! -f "$HOME/.config/zellij/config.kdl" ] && cp "$CONFIG_DIR/zellij/config.kdl" ~/.config/zellij/config.kdl
+[ ! -d "$HOME/.config/zellij/themes" ] && mkdir -p ~/.config/zellij/themes/
 
-cp "$THEME_DIR"/zellij/*.kdl ~/.config/zellij/themes/*
+cp "$THEME_DIR"/zellij/*.kdl ~/.config/zellij/themes/
