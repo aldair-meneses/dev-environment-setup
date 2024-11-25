ed7c4aa008bc7c7158fe51adfdee08f771b52bfa
aldrich <aldrich@debian.debian-vm>
2024-11-24 23:24:26 -0300

    fix: typo in mkdir command

--- apps/eza.sh
+++ apps/eza.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo mkdirr -p /etc/apt/keyrings
+sudo mkdir -p /etc/apt/keyrings
 wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" |
   sudo tee /etc/apt/sources.list.d/gierens.list
