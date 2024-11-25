c763644e1791e93c94dc2a0c3161b742ce980584
aldrich <aldrich@debian.debian-vm>
2024-11-24 22:17:32 -0300

    feat: Adds Ulauncher instalation script

---
+++ apps/ulauncher.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+if [ "$CURRENT_DISTRO" = "Debian GU/Linux" ]; then
+  sudo apt update && sudo apt install -y gnupg
+  gpg --keyserver keyserver.ubuntu.com --recv 0xfaf1020699503176
+  gpg --export 0xfaf1020699503176 | sudo tee /usr/share/keyrings/ulauncher-archive-keyring.gpg >/dev/null
+  echo "deb [signed-by=/usr/share/keyrings/ulauncher-archive-keyring.gpg] \
+          http://ppa.launchpad.net/agornostal/ulauncher/ubuntu jammy main" |
+    sudo tee /etc/apt/sources.list.d/ulauncher-jammy.list
+  sudo apt update && sudo apt install ulauncher
+fi
+
+if [ "$CURRENT_DSITRO" = "Ubuntu" ]; then
+  sudo add-apt-repository universe -y &&
+    sudo add-apt-repository ppa:agornostal/ulauncher -y &&
+    sudo apt update && sudo apt install ulauncher
+fi
