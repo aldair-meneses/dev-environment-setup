d8cb9cb834458484804ebd5ecdcb71ba9719cdb7
Aldair Meneses <aldair@protonmail.com>
2024-11-27 01:06:58 -0300

    refactor: Remove the first ten lines of the bashrc on Node install for all distributions instead of just Ubuntu

--- apps/node/node.sh
+++ apps/node/node.sh
@@ -2,8 +2,6 @@
 
 source ~/.bashrc
 
-if [ "$(sed -n '/^NAME/p' /etc/os-release | tr -d \"NAME=)" = Ubuntu ]; then
-  eval "$(cat ~/.bashrc | tail +10)"
-fi
+eval "$(cat ~/.bashrc | tail +10)"
 
 fnm install --lts
