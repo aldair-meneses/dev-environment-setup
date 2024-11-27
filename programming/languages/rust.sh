ad6232033e4db694d2b13256955b0ae359450c1c
aldrich <aldrich@debian.debian-vm>
2024-11-26 21:19:17 -0300

    refactor: remove condition to enable Rust installation on Ubuntu

--- programming/languages/rust.sh
+++ programming/languages/rust.sh
@@ -1,7 +1,7 @@
 #¡/bin/bash
 set -e
 
-if [ "$CURRENT_DISTRO" = "Debian GNU/Linux" ] && ! command -v rustup &>/dev/null; then
+if ! command -v rustup &>/dev/null; then
   gum style --foreground "#00FF9C" "Rust will be installed"
   bash -c "$(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)" -- -y
 fi
